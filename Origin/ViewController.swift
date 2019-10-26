e419e45acabaf55e48a7c636ff414968c7486b63
Shomil Jain <shomil@avinalabs.com>
2019-10-26 01:07:17 -0700

    Initial Commit

---
+++ Origin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Origin
+//
+//  Created by Shomil Jain on 10/26/19.
+//  Copyright © 2019 Mobile Developers of Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
